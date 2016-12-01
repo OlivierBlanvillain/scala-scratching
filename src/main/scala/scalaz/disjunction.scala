fe129a63470b6bca1451ba5c8c9f3aa6799dc539
Chris James <qui666@gmail.com>
2016-12-01 11:27:15 +0000

    sequenceU is nice

--- src/main/scala/scalaz/disjunction.scala
+++ src/main/scala/scalaz/disjunction.scala
@@ -5,18 +5,26 @@ import scalaz._, Scalaz._
 object Random {
   def queryNextNumber: Exception \/ Long = {
     val source = Math.round(Math.random * 100)
-    if(source < 50) \/.right(source)
+    if(source < 60) \/.right(source)
     else \/.left(new Exception("the generated number is too big"))
   }
 }
 
 object disjunction extends App{
 
-  val numberUnder100 = for {
-    first <- Random.queryNextNumber
-    second <- Random.queryNextNumber
+  import Random._
+
+  // for syntax that i remember
+  val lowishNumbers = for {
+    first <- queryNextNumber
+    second <- queryNextNumber
   } yield(first + second)
 
-  println("numberUnder100 = " + numberUnder100)
+  println("lowish total = " + lowishNumbers)
+
+  // more awesome version
+  val operations = List(queryNextNumber, queryNextNumber, queryNextNumber)
+  val result = operations.sequenceU
+  println("result = " + result)
 
 }
